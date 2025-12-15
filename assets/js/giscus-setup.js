ae5eb41ff961e27a14fb47b78942ba3b7b5e204a
psmiz <psmiz@kaist.ac.kr>
2025-12-15 04:29:50 +0000

    Deploying to gh-pages from @ psmiz/psmiz.github.io@a5cf8d766cb0c3498799dd05f5961095d871fd93 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "psmiz/gitub.io",
+    "data-repo": "psmiz/psmiz.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
